b7101d1699a6c8b1e4e85c426a9861591331c911
cravsky <cravsky@gmail.com>
2025-04-26 23:36:20 +0200

    Added photos

--- src/data/photos.js
+++ src/data/photos.js
@@ -1,16 +1,15 @@
-// Centralized photo mapping
 export const candidatePhotos = {
-    "Joanna Senyszyn": "https://images.pexels.com/photos/3727464/pexels-photo-3727464.jpeg?auto=compress&cs=tinysrgb&w=300",
-    "Marek Jakubiak": "https://images.pexels.com/photos/5668870/pexels-photo-5668870.jpeg?auto=compress&cs=tinysrgb&w=300",
-    "Grzegorz Braun": "https://images.pexels.com/photos/5668858/pexels-photo-5668858.jpeg?auto=compress&cs=tinysrgb&w=300",
-    "Maciej Maciak": "https://images.pexels.com/photos/2182970/pexels-photo-2182970.jpeg?auto=compress&cs=tinysrgb&w=300",
-    "Rafał Trzaskowski": "https://images.pexels.com/photos/5397723/pexels-photo-5397723.jpeg?auto=compress&cs=tinysrgb&w=300",
-    "Artur Bartoszewicz": "https://images.pexels.com/photos/5792641/pexels-photo-5792641.jpeg?auto=compress&cs=tinysrgb&w=300",
-    "Szymon Hołownia": "https://images.pexels.com/photos/5490276/pexels-photo-5490276.jpeg?auto=compress&cs=tinysrgb&w=300",
-    "Woch Marek Marian": "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=300",
-    "Adrian Zandberg": "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=300",
-    "Magdalena Biejat": "https://images.pexels.com/photos/3727513/pexels-photo-3727513.jpeg?auto=compress&cs=tinysrgb&w=300",
-    "Karol Nawrocki": "https://images.pexels.com/photos/2182981/pexels-photo-2182981.jpeg?auto=compress&cs=tinysrgb&w=300",
-    "Krzysztof Stanowski": "https://images.pexels.com/photos/2379875/pexels-photo-2379875.jpeg?auto=compress&cs=tinysrgb&w=300",
-    "Sławomir Mentzen": "https://images.pexels.com/photos/2379876/pexels-photo-2379876.jpeg?auto=compress&cs=tinysrgb&w=300"
-  };
+  "Artur Bartoszewicz": "/candidates/artur_bartoszewicz.jpg",
+  "Magdalena Biejat": "/candidates/magdalena_biejat.jpg",
+  "Grzegorz Braun": "/candidates/grzegorz_braun.jpg",
+  "Szymon Hołownia": "/candidates/szymon_holownia.jpg",
+  "Marek Jakubiak": "/candidates/marek_jakubiak.jpg",
+  "Maciej Maciak": "/candidates/maciej_maciak.jpg",
+  "Sławomir Mentzen": "/candidates/slawomir_mentzen.jpg",
+  "Karol Nawrocki": "/candidates/karol_nawrocki.jpg",
+  "Joanna Senyszyn": "/candidates/joanna_senyszyn.jpg",
+  "Krzysztof Stanowski": "/candidates/krzysztof_stanowski.jpg",
+  "Rafał Trzaskowski": "/candidates/rafal_trzaskowski.jpg",
+  "Marek Marian Woch": "/candidates/marek_marian_woch.jpg",
+  "Adrian Zandberg": "/candidates/adrian_zandberg.jpg"
+};
